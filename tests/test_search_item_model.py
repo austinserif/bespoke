2f6aa0b68e1771ff5c274a8cf0b2f0653db11796
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-06-08 13:58:55 -0700

    deleted test_search_item_model.py, no methods on class, model already covered in other tests

--- tests/test_search_item_model.py
+++
@@ -1,9 +0,0 @@
-"""Production testing file for search item model"""
-
-import os
-from unittest import TestCase
-from app import app
-from models import SearchItem
-
-app.config.from_object('config.Testing')
-db.create_all()
