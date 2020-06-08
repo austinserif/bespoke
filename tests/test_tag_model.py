ddfe5815a32d85711742627aae25b6d7b5f19131
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-06-08 13:56:11 -0700

    added tests to test_tag_model

--- tests/test_tag_model.py
+++ tests/test_tag_model.py
@@ -2,8 +2,36 @@
 
 import os
 from unittest import TestCase
-from app import app
-from models import Tag
+from app import app, db
+from models import User, Tag
 
 app.config.from_object('config.Testing')
 db.create_all()
+
+class UserModelTestCase(TestCase):
+    """Test views for messages."""
+    
+    def setUp(self):
+        """set up test client and seed sample authenticated user"""
+        self.client = app.test_client()
+        self.testuser = User(email="test@test.com", first_name="Test", last_name="User", password="password", authenticated=True)
+        db.session.add(self.testuser)
+        db.session.commit()
+    
+    def tearDown(self):
+        """remove test user"""
+        db.session.delete(self.testuser)
+        db.session.commit()
+    
+    def test_remove_tag(self):
+        """test remove tag method"""
+        tag = User.add_tag("foo", self.testuser.id)
+        self.assertIn(tag, self.testuser.active_tags())
+
+        #now call remove tag
+        Tag.remove_tag(tag.id)
+        self.assertNotIn(tag, self.testuser.active_tags())
+
+        db.session.delete(tag)
+        db.session.commit()
+
