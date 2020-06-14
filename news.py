11b38210dd1f9f4fc4537b1f38e0d0d82c5c85c9
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-06-13 17:32:12 -0700

    add q kwarg to get_everything method

--- news.py
+++ news.py
@@ -16,5 +16,5 @@ def get_articles(search_term):
     """pass search_term into newsapi.get_everything request and return response (2 pages of articles from the past 24 hours)"""
     now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
     yesterday = (datetime.utcnow() - timedelta(hours = 24)).strftime('%Y-%m-%dT%H:%M:%S')
-    all_articles = newsapi.get_everything(qintitle=search_term, language='en', from_param=yesterday, to=now, sort_by='relevancy', page=2)
+    all_articles = newsapi.get_everything(q=search_term, qintitle=search_term, language='en', from_param=yesterday, to=now, sort_by='relevancy', page=2)
     return all_articles
